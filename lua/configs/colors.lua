67af769aac671be01071f6b12c5c9e9704933a89
Nguyen Hua <nguyenhua.hcm@gmail.com>
2023-07-23 16:59:06 +0700

    Update: Modify colors for nvimtree

--- lua/configs/colors.lua
+++ lua/configs/colors.lua
@@ -4,7 +4,8 @@
 vim.cmd[[hi NvimTreeNormal guibg=#1f2529]]
 vim.cmd[[hi NvimTreeEndOfBuffer guibg=#1f2529]]
 vim.cmd[[hi NvimTreeFolderIcon guifg=#A7C080]]
-vim.cmd[[hi VertSplit guifg=#272E33 guibg=NONE]]
+-- vim.cmd[[hi VertSplit guifg=#495156 guibg=NONE]]
+vim.cmd[[hi NvimTreeWinSeparator guifg=#272E33 guibg=NONE]]
 
 -- BufferLine
 vim.cmd[[hi BufferLineFill guibg=#2E383C]]
--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -2,9 +2,11 @@ require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
     width = 28,
+    preserve_window_proportions = true,
   },
   renderer = {
     group_empty = true,
+    root_folder_label = false,
   },
   filters = {
     dotfiles = true,
